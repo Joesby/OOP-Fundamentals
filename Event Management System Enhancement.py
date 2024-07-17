93db6c250f73e87d1e56791750625e18e9e863fe
Joesby <jdlemmon0607@gmail.com>
2024-07-16 19:32:53 -0500

    second commit

--- Event Management System Enhancement.py
+++ Event Management System Enhancement.py
@@ -4,18 +4,24 @@ class Event:
         self.name = name
         self.date = date
         self.num_participants = num_pars
-
+    
+    #add a method for adding participants
     def add_participant(self):
         self.num_participants += 1
 
+    #add a method for getting the number of participants
     def get_participant_count(self):
         return self.num_participants
     
+#create an object for an Event with its own name, date, and number of participants
 party = Event("Office Party", "7-3-24", 3)
 
+#display the current number of participants
 print(party.num_participants)
 
+#run a loop for adding more participants
 for i in range(10):
     party.add_participant()
 
+#display the new number of participants
 print(party.num_participants)
